33c5cbf6cb3f391b4374f5019599fd7f796b4d3b
melikşah tolunay gürcü <87720632+mackali1453@users.noreply.github.com>
2021-07-29 00:51:41 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import router from "./router/router"; 
 
 
-let express = require('express'); #express is a modul which allow us to create rest-api.
-let app = express(); #Creating instance of it
-app.use("/",router); #Using router let us jumping between html pages.
+let express = require('express'); //express is a modul which allow us to create rest-api.
+let app = express(); //Creating instance of it
+app.use("/",router); //Using router let us jumping between html pages.
 app.listen(3000,console.log("localhost is 3000")); 
 
