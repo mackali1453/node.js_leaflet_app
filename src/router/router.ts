6f68744da3d6fcc00b4ed3b13c58262e416ab292
melikşah tolunay gürcü <87720632+mackali1453@users.noreply.github.com>
2021-07-29 00:57:06 +0300

    Update router.ts

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,11 +1,11 @@
 import postController from "../postController/post";
 import {Router} from "express";
-const controller = new postController();
-const router = Router();
+const controller = new postController(); //postController class includes functions.
+const router = Router(); //Router class allow us to associate our functions which have http methods to different routes.
 
-router.all("/leaflet",controller.map);
-router.all("/calendar",controller.calendar);
-router.all("/login",controller.login);
+router.all("/leaflet",controller.map); //routes to leaflet map page
+router.all("/calendar",controller.calendar); //routes to calendar page
+router.all("/login",controller.login);//routes to login page 
 
 
 
